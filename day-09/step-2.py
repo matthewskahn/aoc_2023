0f67500bb01f5383c5e904a2e7f682b3fba976a6
Matthew Kahn <matthewkahn@gmail.com>
2023-12-09 10:34:31 -0600

    Day 9

---
+++ day-09/step-2.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import re
+
+with open(f'{Path(__file__).resolve().parent}/input.txt', 'r') as f:
+    lines = [l.strip() for l in f]
+
+lines = [list(map(lambda x: int(x), re.findall(r'-?\d+', line))) for line in lines]
+
+def derivative(arr):
+    next = 0
+    deltas = [arr[i] - arr[i - 1] for i in range(1, len(arr))]
+    
+    if all(n == 0 for n in deltas):
+        return arr[0]
+    else:
+        next = derivative(deltas)
+    return arr[0] - next
+
+print(sum([derivative(val) for val in lines]))
